8455197ddb463d8f666ee73d39d7c78acb9ec0ef
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-05 01:43:07 +0900

    Add constants/errors, messages, tokenInfos

---
+++ constants/errors.js
@@ -0,0 +1,4 @@
+const TOKEN_EXPIRED_ERROR = "TokenExpiredError";
+const JSON_WEB_TOKEN_ERROR = "JsonWebTokenError";
+
+module.exports = { TOKEN_EXPIRED_ERROR, JSON_WEB_TOKEN_ERROR };
---
+++ constants/tokenInfos.js
@@ -0,0 +1,7 @@
+const ISSUER = "onCondition-admin";
+const ACCESS_DURATION = "1000ms";
+const REFRESH_DURATION = "7d";
+
+module.exports = {
+  ISSUER, ACCESS_DURATION, REFRESH_DURATION
+};
