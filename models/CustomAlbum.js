a475401f4b7d34796c222451f17f8279c81536d7
Soodev20 <soodev20@gmail.com>
2021-09-06 03:02:31 +0900

    Chore: subtract & merge rating schema

--- models/CustomAlbum.js
+++ models/CustomAlbum.js
@@ -1,16 +1,5 @@
 const mongoose = require("mongoose");
-
-const ratingSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-  },
-  heartCount: {
-    type: Number,
-  },
-  text: {
-    type: String,
-  },
-});
+const ratingSchema = require("./subDocuments/Rating");
 
 const customAlbumSchema = new mongoose.Schema({
   category: {
