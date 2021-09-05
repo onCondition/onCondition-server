8174d5fb56a3bf7a5485f7655fc09d95c8a6fd53
Soodev20 <soodev20@gmail.com>
2021-09-05 23:57:12 +0900

    Add: more error messaages as constants

--- constants/messages.js
+++ constants/messages.js
@@ -1,6 +1,15 @@
 const ERROR = {
   TOKEN_EXPIRED: "로그인이 만료되었습니다. 다시 로그인해주세요.",
   REQUIRE_LOGIN: "로그인이 필요한 서비스입니다.",
+  INTERNAL_SERVER_ERROR: "현재 서버 이용이 불가능합니다.",
+  INVALID_URL: "유효하지 않은 URL입니다",
+  INVALID_DATE: "유효하지 않은 날짜입니다",
+  INVALID_HEART_COUNT: "유효하지 않은 만족도입니다",
+  INVALID_RATING_TEXT: "유효하지 않은 평가문입니다",
+  INVALID_TOKEN: "유효하지 않은 토큰입니다.",
+  INVALID_USING_MAX_CATEGORIES: "이미 최대 3개의 커스텀 카테고리를 모두 사용하고 있습니다.",
+  INVALID_OVERLAP_CATEGORY_NAME: "이미 사용중인 카테고리 이름입니다.",
+  INVALID_ALREADY_DELETED_CATEGORY: "이미 삭제된 카테고리입니다.",
 };
 
 module.exports = { ERROR };
