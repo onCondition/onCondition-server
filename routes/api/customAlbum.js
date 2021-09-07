cb9a59cf7609e292e54688a880497679cf22d906
Soodev20 <soodev20@gmail.com>
2021-09-08 02:08:55 +0900

    Add: customAlbum router

---
+++ routes/api/customAlbum.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const albumController = require("../controller/customAlbum");
+const requiresLogin = require("../middleware/requiresLogin");
+
+router.get("/", requiresLogin, albumController.getAlbum);
+
+router.post("/", albumController.postAlbum);
+
+router.get("/:id", albumController.getAlbumDetail);
+
+router.patch("/:id", albumController.patchAlbumDetail);
+
+router.delete("/:id", albumController.deleteAlbumDetail);
+
+module.exports = router;
