9299526bd0c799ff2136e4810cca65a7ed96176a
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-08 03:16:09 +0900

    Add: Sleep schema

---
+++ models/Sleep.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const ratingSchema = require("./subDocuments/Rating");
+
+const sleepSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: "hashed",
+  },
+  sessionId: {
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+  },
+  rating: ratingSchema,
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+  }],
+});
+
+sleepSchema.path("_id");
+sleepSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Sleep", sleepSchema);
