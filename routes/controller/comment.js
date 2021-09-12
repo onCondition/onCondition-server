0f2fe0a53eee2a93ece1e3da6aed35288281d38f
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-13 00:18:03 +0900

    Add: comment post

--- routes/controller/comment.js
+++ routes/controller/comment.js
@@ -1,9 +1,85 @@
-const { OK } = require("../../constants/statusCodes");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
 
-function postComment(req, res, next) {
+const Comment = require("../../models/Comment");
+const Activity = require("../../models/Activity");
+const Album = require("../../models/CustomAlbum");
+const Grid = require("../../models/CustomGrid");
+const Meal = require("../../models/Meal");
+const Sleep = require("../../models/Sleep");
+
+const ACCESS_LEVELS = require("../../constants/accessLevels");
+const {
+  OK, BAD_REQUEST, NOT_FOUND, UNAUTHORIZED,
+} = require("../../constants/statusCodes");
+const { ERROR } = require("../../constants/messages");
+
+const RatingModels = {
+  Activity,
+  Album,
+  Grid,
+  Meal,
+  Sleep,
+};
+
+const categoryModelName = {
+  activity: "Activity",
+  album: "CustomAlbum",
+  grid: "CustomGrid",
+  meal: "Meal",
+  sleep: "Sleep",
+};
+
+function getCategoryType(categoryName, customCategories) {
+  if (["activity", "meal", "sleep"].includes(categoryName)) {
+    return categoryName;
+  }
+
+  const categoryInfo = customCategories.find(
+    ({ category }) => category === categoryName,
+  );
+
+  if (!categoryInfo) {
+    return null;
+  }
+
+  return categoryInfo.category;
+}
+
+async function postComment(req, res, next) {
+  const { creator, userId } = req;
+  const { category, ratingId } = req.params;
+  const { date, content } = req.body;
+  const categoryType = getCategoryType(category, creator.customCategories);
+  const modelName = categoryModelName[categoryType];
+
   try {
+    if (req.accessLevel === ACCESS_LEVELS.GUEST) {
+      throw createError(UNAUTHORIZED);
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(ratingId)) {
+      throw createError(NOT_FOUND);
+    }
+
+    if (!categoryType) {
+      throw createError(BAD_REQUEST, ERROR.CATEGORY_NOT_FOUND);
+    }
+
+    const newComment = await Comment.create({
+      category: modelName,
+      ratingId,
+      creator: userId,
+      date,
+      content,
+    });
+
+    const commentId = newComment._id;
+    await RatingModels[modelName].findByIdAndUpdate(ratingId,
+      { $push: { comments: [commentId] } });
+
     res.status(OK);
-    res.json({ result: "ok", category: req.category });
+    res.json({ result: "ok" });
   } catch (err) {
     next(err);
   }
