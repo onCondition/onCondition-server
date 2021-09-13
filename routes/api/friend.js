4364df80983a4ea18ec33cd267a41dda465826ae
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-13 12:21:04 +0900

    Modify: friends profile route

--- routes/api/friend.js
+++ routes/api/friend.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const friendController = require("../controller/friend");
+const indexController = require("../controller/index");
 
 router.get("/", friendController.getFriends);
+
+router.get("/:id", indexController.getProfile);
 
 router.post("/new", friendController.sendFriendRequest);
 
