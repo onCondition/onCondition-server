f78fe66080c489cb07d1a4358c9957b56304e0be
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-14 15:20:08 +0900

    Modify: googleFit accessToken flow

--- routes/controller/googleFit.js
+++ routes/controller/googleFit.js
@@ -11,11 +11,11 @@ const { OK, UNAUTHORIZED } = require("../../constants/statusCodes");
 
 async function postGoogleToken(req, res, next) {
   try {
+    const accessToken = req.cookies.googleAccessToken;
+
     if (req.accessLevel !== ACCESS_LEVELS.CREATOR) {
       throw createError(UNAUTHORIZED);
     }
-
-    const { accessToken } = req.body;
 
     if (!accessToken) {
       return next(createError(UNAUTHORIZED, ERROR.INVALID_TOKEN));
@@ -26,13 +26,13 @@ async function postGoogleToken(req, res, next) {
     try {
       const steps = await getGoogleFitStepData(accessToken);
 
-      await updateModels({ activities, sleeps, steps }, req.creator);
+      await updateModels({ activities, sleeps, steps }, req.creator.id);
 
       res.status(OK);
       res.json({ result: "ok" });
     } catch (err) {
       if (err.message === ERROR.STEP_DATA_NOT_AVAILABLE) {
-        await updateModels({ activities, sleeps }, req.creator);
+        await updateModels({ activities, sleeps }, req.creator.id);
 
         res.status(OK);
         res.json({ result: "No Step Data" });
